b68afac48968a7ef1b4d8f9651246cd9e3a2172a
huihuicloud <chenrichardit@gmail.com>
2022-11-16 17:29:57 +0100

    maj

---
+++ main.js
@@ -0,0 +1,60 @@
+const {app, BrowserWindow, Menu, ipcMain} = require('electron')
+const url = require('url')
+const path = require('path')
+
+const options = false;
+
+Menu.setApplicationMenu(false)
+
+function createWindow () { 
+    if(options == true){
+        const mainWindow = new BrowserWindow({
+            show: false,
+            height: 440,
+            width: 753,
+            webPreferences: {
+                nodeIntegration: true,
+                enableRemoteModule: true
+            },
+            frame: false,
+            maximizable: false,
+            minimizable: false,
+            title: 'HCR',
+        });
+        mainWindow.loadURL('http://localhost:4200')
+        mainWindow.once('ready-to-show', () => {
+            mainWindow.show();
+        });
+    }else{
+        const mainWindow = new BrowserWindow({
+            show: false,
+            height: 440,
+            width: 753,
+            webPreferences: {
+                nodeIntegration: true,
+                enableRemoteModule: true
+            },
+            frame: false,
+            maximizable: false,
+            minimizable: false,
+            title: 'HCR',
+        });
+        mainWindow.loadURL(url.format({pathname: path.join(__dirname, `/dist/hcr_release/index.html`),protocol: "file:",slashes: true}));
+        mainWindow.once('ready-to-show', () => {
+            mainWindow.show();
+        });
+    }
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.allowRendererProcessReuse = false
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+const uaup = (window as any).require('uaup-js');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hcr_release';
+export class AppComponent implements AfterViewInit{
+
+  ngAfterViewInit(){
+    const defaultStages = {
+      Checking: "Initialisation", // When Checking For Updates.
+      Found: "Recherche des mises a jour",  // If an Update is Found.
+      NotFound: "Logiciel est a jour", // If an Update is Not Found.
+      Downloading: "En cours de telechargement", // When Downloading Update.
+      Unzipping: "En cours d'installation", // When Unzipping the Archive into the Application Directory.
+      Cleaning: "Optimisation", // When Removing Temp Directories and Files (ex: update archive and tmp directory).
+      Launch: "Demarrage" // When Launching the Application. 
+    };
+    const updateOptions = {
+      gitUsername: "huihuicloud",  // [Required] Your GitHub Username.
+      gitRepo: "hcr_release", // [Required] Your Repo Name
+    
+      appName: "hcr", //[Required] The Name of the app archive and the app folder.
+      appExecutableName: "hcr.exe", //[Required] The Executable of the Application to be Run after updating.
+    
+      progressBar: document.getElementById("download"), // {Default is null} [Optional] If Using Electron with a HTML Progressbar, use that element here, otherwise ignore
+      label: document.getElementById("download-label"), // {Default is null} [Optional] If Using Electron, this will be the area where we put status updates using InnerHTML
+      stageTitles: defaultStages, // {Default is defaultStages} [Optional] Sets the Status Title for Each Stage
+    };
+    uaup.Update(updateOptions);
+  }
 }
